819222830a0b505ed1ca1d6be7317166ce9f1841
Romanv1812 <romanv1812@gmail.com>
2022-05-25 18:15:07 +0300

    main

---
+++ 5-variables/1-basic-data-types/1-print-the-literals/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Integer literals: ", -1, ",", 0, ",", 1)
+	fmt.Println("Float literals: ", -1.5, ",", .0, ",", 1.5)
+	fmt.Println("Bool constants: ", true, ",", false)
+	fmt.Println("My name is: ", "Roman")
+	fmt.Println("My name is: ", "Rçman")
+}
