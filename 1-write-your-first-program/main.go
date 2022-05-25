9857712bef3e7cdaccb1459aa8481b2ab7300336
Romanv1812 <romanv1812@gmail.com>
2022-05-25 14:27:26 +0300

    Done

---
+++ 1-write-your-first-program/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Print names
+//
+//  Print your name and your best friend's name using
+//  Println twice
+//
+// EXPECTED OUTPUT
+//  YourName
+//  YourBestFriendName
+//
+// BONUS
+//  Use `go run` first.
+//  And after that use `go build` and run your program.
+// ---------------------------------------------------------
+
+func main() {
+	fmt.Println("My name is: ", "Roman")
+	fmt.Println("My best friend name is: ", "Name")
+}
