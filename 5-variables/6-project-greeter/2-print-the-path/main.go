9ebecd74fdb46b68f723d3571ad9466030f07261
Romanv1812 <romanv1812@gmail.com>
2022-05-31 13:14:45 +0300

    main

---
+++ 5-variables/6-project-greeter/2-print-the-path/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Print the Path
+//
+//  Print the path of the running program by getting it
+//  from `os.Args` variable.
+//
+// HINT
+//  Use `go build` to build your program.
+//  Then run it using the compiled executable program file.
+//
+// EXPECTED OUTPUT SHOULD INCLUDE THIS
+//  myprogram
+// ---------------------------------------------------------
+
+func main() {
+	fmt.Printf("File name is: %s\n", os.Args[0])
+}
