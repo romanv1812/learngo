2efb60f5a0fbff387df349bf83245a5c7cdf940a
Romanv1812 <romanv1812@gmail.com>
2022-06-01 17:05:19 +0300

    main

---
+++ 6-printf/2-print-your-name-and-lastname/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Print Your Name and LastName
+//
+//  Print your name and lastname using Printf
+//
+// EXPECTED OUTPUT
+//  My name is Inanc and my lastname is Gumus.
+//
+// BONUS
+//  Store the formatting specifier (first argument of Printf)
+//    in a variable.
+//  Then pass it to printf
+// ---------------------------------------------------------
+
+func main() {
+	fmt.Printf("My name is %s and my lastname is %s.\n", "Roman", "Neporozhnii")
+
+	msg := "My name is %s and my lastname is %s.\n"
+
+	fmt.Printf(msg, "Roman", "Neporozhnii")
+
+}
---
+++ 6-printf/2-print-your-name-and-lastname/readme.md
@@ -0,0 +1,13 @@
+ ---------------------------------------------------------
+ EXERCISE: Print Your Name and LastName
+
+  Print your name and lastname using Printf
+
+ EXPECTED OUTPUT
+  My name is Inanc and my lastname is Gumus.
+
+ BONUS
+  Store the formatting specifier (first argument of Printf)
+    in a variable.
+  Then pass it to printf
+ ---------------------------------------------------------
