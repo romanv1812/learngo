016b44553a4b5458b35589389a75c693ae0a80f9
Romanv1812 <romanv1812@gmail.com>
2022-06-13 19:36:41 +0300

    main

---
+++ 8-go-type-system/5-refactor-feet-to-meter/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Refactor Feet to Meter
+//
+//  Remember the feet to meters program?
+//  Now, it's time to refactor it.
+//  Define your own Feet and Meters types.
+//
+//  Follow the steps inside the code.
+// ---------------------------------------------------------
+
+func main() {
+	// ----------------------------
+	// 1. Define Feet and Meters types below
+	//    Their underlying type can be float64
+	type (
+		Feet   float64
+		Meters float64
+	)
+
+	// ----------------------------
+	// 2. UNCOMMENT THE CODE BELOW THEN DON'T TOUCH IT
+	var (
+		feet   Feet
+		meters Meters
+	)
+
+	// ----------------------------
+	// 3. Get feet value from the command-line
+	// 4. Convert it to an float64 first using ParseFloat
+	// 5. Then, convert it into a Feet type
+	val, _ := strconv.ParseFloat(os.Args[1], 64)
+	feet = Feet(val)
+
+	// ----------------------------
+	// 6. Uncomment the code below
+	// 7. And, convert the expression to Meters type
+
+	meters = Meters(feet * 0.3048)
+
+	// ----------------------------
+	// 8. UNCOMMENT THE CODE BELOW
+	fmt.Printf("%g feet is %g meters.\n", feet, meters)
+}
