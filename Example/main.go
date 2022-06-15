3d82cb66c4c56ed36a97e992c75c699238e609cb
Romanv1812 <romanv1812@gmail.com>
2022-06-15 16:37:08 +0300

    main

--- Example/main.go
+++ Example/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const meters int = 100
+
+	cm := 100
+	m := cm / meters
+	fmt.Printf("%dcm is %dm\n", cm, m)
+	cm = 200
+	m = cm / meters
+	fmt.Printf("%dcm is %dm\n", cm, m)
+
+	const (
+		total, number0f int = 5, 1
+	)
+	fmt.Println(total / number0f)
+}
